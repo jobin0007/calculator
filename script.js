7512e2074eed47f6d2df50b37954ae9662632ff2
jobin0007 <jobinmj727@gmil.com>
2021-11-28 20:16:08 +0530

    intial

---
+++ script.js
@@ -0,0 +1,32 @@
+var displayValues=(num)=>result.value+=num
+// function displayValues(num)
+// {
+//     let res=document.querySelector("#result")
+//     res.value+=num;
+// }
+var clearBox=()=>result.value=""
+// function clearBox()
+// {
+//     let res=document.querySelector("#result")
+//     res.value=""
+// }
+var backSpace=()=>result.value=result.value.slice(0,-1)
+// function backSpace()
+// {
+//     let res=document.querySelector("#result")
+//    let cur_value=res.value
+//    res.value=cur_value.slice(0,-1)
+
+
+// }
+var evalauateExpression=()=>result.value=eval(result.value)
+// function evalauateExpression()
+// {
+    
+//     let res=document.querySelector("#result")
+//     let cur_value=res.value
+//     res.value=cur_value
+//     let result=eval(cur_value)
+//     res.value=result
+
+// }
